2340316eaf840ee6eb218ee98d097ffabd51707d
Gabriel Maiolli <bielmaiolli@gmail.com>
2024-09-07 23:18:17 -0300

    Add files via upload

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignUpDto } from 'src/deck/dto/signup.dto';
 import { LoginDto } from 'src/deck/dto/login.dto';
+import { User } from './schemas/user.schema';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -14,7 +16,46 @@ export class AuthController {
     }
 
     @Get('/login')
+    @UseGuards(AuthGuard())
     login( @Body() loginDto: LoginDto): Promise<{ token: string }> {
         return this.authService.login(loginDto);
     }
+
+//=========================================================
+
+   @Get('allUsers')
+   async getAllUsers(): Promise<User[]> {
+     return this.authService.findAll();
+   }   
+
+   @Get(':id')
+   async getById(
+    @Param('id')
+    id: string,
+   ): Promise<User> {
+     return this.authService.findById(id);
+   }
+
+   @Put('updateUser/:id')
+   @UseGuards(AuthGuard())
+   async updateUser(
+     @Param('id')
+     id: string,
+     @Body()
+     user: User,
+   ): Promise<User> {
+     return this.authService.updateById(id, user);
+   }
+
+
+   @Delete('deleteUser/:id')
+   @UseGuards(AuthGuard())
+   async deleteById(
+        @Param('id')
+        id: string,
+    ): Promise<User> {
+        return this.authService.deleteById(id);
+    }
+
+
 }
