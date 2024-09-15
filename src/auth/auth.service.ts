39eace4c324f61c8a71876137b15366371e1c180
Gabriel Maiolli <bielmaiolli@gmail.com>
2024-09-15 14:21:32 -0300

    Add files via upload

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { JwtService } from "@nestjs/jwt";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { User } from "./schemas/user.schema";
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { Injectable, NotFoundException, UnauthorizedException } from "@nestjs/common";
 import { SignUpDto } from "src/deck/dto/signup.dto";
 import * as bcrypt from 'bcryptjs';
 import { LoginDto } from "src/deck/dto/login.dto";
@@ -56,5 +56,40 @@ export class AuthService {
      return { token }
    } 
    
+ //-----------------------------------------------------------
 
+ async findAll(): Promise<User[]> {
+   const users = await this.userModel.find();
+   return users;
+}
+
+
+async create(user: User): Promise<User> {
+   const res = await this.userModel.create(user);
+   return res;
+}
+
+
+async findById(id: string): Promise<User> {
+   const user = await this.userModel.findById(id);
+
+   if(!user){
+      throw new NotFoundException('O User não foi encontrado');
+   }
+
+   return user;
+}
+
+
+async updateById(id: string, user: User): Promise<User> {
+   return await this.userModel.findByIdAndUpdate(id, user, {
+      new: true,
+      runValidators: true,
+   });
+}
+
+
+async deleteById(id: string): Promise<User> {
+   return await this.userModel.findByIdAndDelete(id);
+}
 }
