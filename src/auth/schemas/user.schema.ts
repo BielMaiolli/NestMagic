51a306f3d3042588b275db618b0bca89c623eba6
Gabriel Maiolli <bielmaiolli@gmail.com>
2024-09-15 13:47:28 -0300

    Add files via upload

--- src/auth/schemas/user.schema.ts
+++ src/auth/schemas/user.schema.ts
@@ -1,6 +1,5 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-
-
+import { Role } from "../enums/role.enum";
 
 @Schema({
     timestamps: true,
@@ -17,6 +16,12 @@ export class User {
     @Prop({ required: true })
     password: string;
 
+    @Prop({
+        type: [{type: String, enum: Role}],
+        default: [Role.USER]
+    })
+    role: Role[];
+
 }
 
-export const UserSchema = SchemaFactory.createForClass(User)
+export const UserSchema = SchemaFactory.createForClass(User);
